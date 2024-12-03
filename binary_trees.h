6a5589f4d477873f4637f8398b1614ae2edc6b71
peter david <princewill016@gmail.com>
2024-12-02 22:03:42 -0800

    done

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,7 +19,43 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function prototype for creating a binary tree node */
+/* Function to create a binary tree node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* Function to insert a node as the left-child of another node */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* Function to insert a node as the right-child of another node */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* Function to delete an entire binary tree */
+void binary_tree_delete(binary_tree_t *tree);
+
+/* Function to check if a node is a leaf */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* Function to check if a node is a root */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* Function for pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Function for in-order traversal */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Function for post-order traversal */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Function to measure the height of a binary tree */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* Function to measure the depth of a node in a binary tree */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* Function to measure the size of a binary tree */
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/* Function to count nodes with at least 1 child in a binary tree */
+size_t binary_tree_nodes(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
